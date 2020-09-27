329903b7da627e8e230f65d2bd6ee96bc6467acd
Mahato <abhimanyu.mahato@accenture.com>
2020-09-27 17:27:47 +0530

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='mrktmix',
-    version='0.1.0',
+    version='0.1.1',
     license='LGPL-3.0-or-later',
     description='Market Mix Modeling',
     long_description='%s\n%s' % (
--- src/mrktmix/__init__.py
+++ src/mrktmix/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 from mrktmix.transformation import apply_apl
 from mrktmix.transformation import apply_coef
 from mrktmix.transformation import assess_error
