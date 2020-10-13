82b1310f88d99c0c5f1ce25ff8e9153d528db4ce
540pd <apd_540@hotmail.com>
2020-10-13 23:01:59 +0530

    Bump version: 0.1.2 → 0.1.3

--- src/mrktmix/__init__.py
+++ src/mrktmix/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 from mrktmix.optimization import optimize_spend
 from mrktmix.transformation import apply_apl
 from mrktmix.transformation import apply_coef
