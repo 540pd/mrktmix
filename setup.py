ea72a0bb5f92536f234c01b49adb3965361d041e
540pd <apd_540@hotmail.com>
2020-11-29 14:59:33 +0530

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='mrktmix',
-    version='0.1.10',
+    version='0.1.11',
     license='LGPL-3.0-or-later',
     description='Market Mix Modeling',
     long_description='%s\n%s' % (
--- src/mrktmix/__init__.py
+++ src/mrktmix/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 from mrktmix.data_create import aggregate_rows
 from mrktmix.data_create import create_mdldata
 from mrktmix.data_create import parse_variable
