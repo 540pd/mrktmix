51469de20276d0a8fe3dadc736630e9c7e1052a3
540pd <apd_540@hotmail.com>
2021-06-27 12:49:29 +0530

    update optimization func

--- src/mrktmix/__init__.py
+++ src/mrktmix/__init__.py
@@ -5,7 +5,7 @@ from mrktmix.data_create import parse_variable
 from mrktmix.data_create import reserve_dict
 from mrktmix.data_create import spread_notna
 from mrktmix.data_create import update_description
-from mrktmix.optimization import optimize_spend
+from mrktmix.optimization import mmm_optimize
 from mrktmix.transformation import aggregate_data
 from mrktmix.transformation import apply_apl
 from mrktmix.transformation import apply_coef
